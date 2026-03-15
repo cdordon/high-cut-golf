67131d2fd13db46d4668ae1fe363eaa122d788b9
cdordon <121517463+cdordon@users.noreply.github.com>
2026-03-15 13:37:38 -0400

    Add multi-page routing

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        hats: resolve(__dirname, 'hats.html'),
+        shirts: resolve(__dirname, 'shirts.html'),
+        accessories: resolve(__dirname, 'accessories.html'),
+      },
+    },
+  },
+})
