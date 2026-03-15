05890aecb2bc2a53a011b58bb87e73d9a51564d5
cdordon <121517463+cdordon@users.noreply.github.com>
2026-03-15 16:16:53 -0400

    updated about and privacy to public

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about.html'),
         hats: resolve(__dirname, 'hats.html'),
         shirts: resolve(__dirname, 'shirts.html'),
         accessories: resolve(__dirname, 'accessories.html'),
-        privacy: resolve(__dirname, 'privacy.html'), // ADD THIS LINE
-        404: resolve(__dirname, '404.html'),
       },
     },
   },
